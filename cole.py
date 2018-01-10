76f6ea651a8baa5e1c2ea353f5f0922a2c4abbc4
cole <me@colexd.us>
2018-01-10 01:18:34 -0800

    variables

--- cole.py
+++ cole.py
@@ -4,3 +4,8 @@ print('Hello world!')
 
 # user input wowo
 print(input('Enter your name: ') + ' smells bad')
+
+# variables owo
+character = input('Enter character name: ') + '.chr'
+del character
+print(character)
